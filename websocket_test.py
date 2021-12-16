3baa9bc5cc9ddbb767b40813ab31101c50165dad
Tanjil Hasan <41756334+tanjilh136@users.noreply.github.com>
2021-12-16 10:44:40 +0100

    Update websocket_test.py

--- websocket_test.py
+++ websocket_test.py
@@ -7,7 +7,7 @@ def test_on_msg(msg):
 
 
 if __name__ == "__main__":
-    key = "121212"
+    key = ""
     websocket_client = WebSocketClientClone(uri=f"ws://localhost:8000/api/ws/{key}")
     websocket_client.attach_on_msg_listener(test_on_msg)
     websocket_client.run_async()
